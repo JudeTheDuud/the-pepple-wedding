b25a28794c1087585686fa10f50f996cac3f7472
JudeTheDuud <praiseosai@gmail.com>
2024-03-06 15:31:05 +0100

    fx/ navbar-adjustments-and-animations
    -added event listener for hiding navbar after page has passed a certain scroll threshold
    -said event listener also shows navbar if user scrolls up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { ReactComponent as Ham } from "../assets/svgs/ham.svg";
 import "../styles/navbar.css";
@@ -10,16 +10,55 @@ const Navbar = () => {
     setShowNavbar(!showNavbar);
   };
 
+  const [scrollData, setScrollData] = useState({
+    y: 0,
+    lastY: 0,
+  });
+
+  const [showNav, setshowNav] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollData((prevState) => ({
+        ...prevState,
+        y: window.scrollY,
+        lastY: prevState.y,
+      }));
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollData.y > 800) {
+      setshowNav(true);
+    } else {
+      setshowNav(false);
+    }
+    
+    if (scrollData.y  < scrollData.lastY ) {
+      setshowNav(true);
+    } else {
+      setshowNav(false);
+    }
+
+
+  }, [scrollData]);
+
   return (
-    <div>
+    <div className={showNav ? 'nav' : 'hideNav'}>
       <nav className="navbar">
         <div className="container">
-        <div className="menu-icon" onClick={handleShowNavbar}>
-            <Ham  className="ham"/>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <Ham className="ham" />
           </div>
           <div className="logo">JM</div>
-          
-          <div className={`navElements ${showNavbar && 'active'}`}>
+
+          <div className={`navElements ${showNavbar && "active"}`}>
             <ul>
               <li>
                 {" "}
